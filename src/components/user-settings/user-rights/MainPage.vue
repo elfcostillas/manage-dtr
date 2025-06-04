959d810b79c177baf20525c9cd354b85f7fbe625
Elmer <elfcostillas@gmail.com>
2025-06-04 17:00:34 +0800

    manage user

--- src/components/user-settings/user-rights/MainPage.vue
+++ src/components/user-settings/user-rights/MainPage.vue
@@ -1,11 +1,6 @@
 <template>
     <div class="flex gap-2 justify-center">
-        <Card style="width:auto;">
-                <template #title>User Rights </template>
-                <template #content>
-                    <TablePage></TablePage>
-                </template>
-        </Card>
+        
     </div>
 </template>
 
